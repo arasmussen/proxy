21eb64229725a3d6b09f26d8208a2fd649206259
Andrew <arasmussen@katworks.com>
2014-02-17 22:35:27 -0800

    add bitcoin stuff

--- proxy.js
+++ proxy.js
@@ -2,11 +2,12 @@ var httpProxy = require('http-proxy');
 
 var options = {
   router: {
+    '.*\.venz\.io': '127.0.0.1:8001',
     'venz.io': '127.0.0.1:8001',
-    'www.venz.io': '127.0.0.1:8001',
     'rasmuzen.com': '127.0.0.1:8002',
     'www.rasmuzen.com': '127.0.0.1:8002',
-    'trigrid.rasmuzen.com': '127.0.0.1:8003'
+    'trigrid.rasmuzen.com': '127.0.0.1:8003',
+    'coin.rasmuzen.com': '127.0.0.1:8004',
   }
 };
 
