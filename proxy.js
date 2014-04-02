29216246976ef9d105a73eb05663eea7b2cc86a4
Andrew <arasmussen@katworks.com>
2014-04-02 01:05:12 -0700

    support https and redirect http for 60L

--- proxy.js
+++ proxy.js
@@ -1,17 +1,38 @@
+var fs = require('fs');
 var httpProxy = require('http-proxy');
 
-var options = {
+var httpOptions = {
   router: {
     '.*\.venz\.io': '127.0.0.1:8001',
     'venz.io': '127.0.0.1:8001',
     'rasmuzen.com': '127.0.0.1:8002',
     'www.rasmuzen.com': '127.0.0.1:8002',
     'trigrid.rasmuzen.com': '127.0.0.1:8003',
-    'coin.rasmuzen.com': '127.0.0.1:8004',
-    'www.sixtylarge.com': '127.0.0.1:8004',
-    'sixtylarge.com': '127.0.0.1:8004',
+    'www.sixtylarge.com': '127.0.0.1:8005',
+    'sixtylarge.com': '127.0.0.1:8005'
   }
 };
 
-var proxyServer = httpProxy.createServer(options);
-proxyServer.listen(80);
+var httpProxyServer = httpProxy.createServer(httpOptions);
+httpProxyServer.listen(80);
+
+var httpsOptions = {
+  https: {
+    cert: fs.readFileSync('../../.ssl/www.sixtylarge.com/cert'),
+    key: fs.readFileSync('../../.ssl/www.sixtylarge.com/key')
+  },
+  router: {
+    'www.sixtylarge.com': '127.0.0.1:8005',
+    'sixtylarge.com': '127.0.0.1:8004'
+  }
+};
+
+var httpsProxyServer = httpProxy.createServer(httpsOptions);
+httpsProxyServer.listen(443);
+
+// redirect [http://] || [www.] to https://sixtylarge.com
+var express = require('express');
+var http = express();
+http.get('*', function(req, res) {
+  res.redirect('https://sixtylarge.com' + req.url);
+}).listen(8005);
